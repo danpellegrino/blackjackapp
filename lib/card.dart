63643ba0cb9877544838e73a62dea830da9c0c8d
danpellegrino <danpellegrino1@gmail.com>
2022-05-24 02:16:54 -0400

    added ui

--- lib/card.dart
+++ lib/card.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
-class MyCard extends StatelessWidget {
+class CardBack extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
           child: Container(
             color: Colors.white,
             height: 150,
             width: 100,
-          )
+            child: Image.asset('lib/assets/blackcard_back.jpg'),
+          ),
+      ),
     );
   }
 }
--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,23 +1,88 @@
 import 'package:flutter/material.dart';
+
+import 'dart:math';
 import 'card.dart';
+import 'cardtemplate.dart';
+import 'suits.dart';
 
 class Homepage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<Homepage> {
+class _HomePageState extends State<Homepage> with SingleTickerProviderStateMixin {
+
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  AnimationStatus _animationStatus = AnimationStatus.dismissed;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController =
+        AnimationController(duration: Duration(milliseconds: 200), vsync: this);
+
+    _animation = Tween(begin:0.0, end: 1.0).animate(_animationController)
+      ..addListener(() {
+      setState(() {});
+    })..addStatusListener((status) {_animationStatus = status;});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[900],
       body: Center(
-        child: Container(child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            MyCard()
-          ],
-        ),),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [CardTemplate(
+                  color: Colors.black,
+                  number: '10',
+                  suit: clover(),
+                ), CardTemplate(
+                  color: Colors.red,
+                  number: 'J',
+                  suit: heart(),
+                )],
+              ),
+              Transform.rotate(angle: pi/2, child: CardBack(),),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  getFlippingCard("Q", clover(), Colors.black),
+                  getFlippingCard("A", diamond(), Colors.red)
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getFlippingCard(var number, var suit, var color) {
+    return Transform(
+      alignment: FractionalOffset.center,
+      transform: Matrix4.identity()..setEntry(3, 2, 0.002)..rotateY(pi * _animation.value),
+      child: GestureDetector(
+        onTap: () {
+          if (_animationStatus == AnimationStatus.dismissed) {
+            _animationController.forward();
+          }
+          else {
+            _animationController.reverse();
+          }
+        },
+        child: _animation.value >= 0.5 ? CardBack() : CardTemplate(
+                  color: color,
+                  number: number,
+                  suit: suit,
+                ),
       ),
     );
   }
